1c7e77b2afff30b5be04c7e3dd28138a9bf7854d
AdarshDsGit <adarshtenzo@gmail.com>
2023-04-23 12:40:06 +0530

    Data ingestion and Transformer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
+
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str= os.path.join("artifacts","train.csv")
@@ -17,7 +24,7 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered teh data ingestion method or component")
+        logging.info("Started the data ingestion method or component")
         try:
             df = pd.read_csv('notebook\data\students.csv')
             logging.info('Exported or Read the dataset as DataFrame')
@@ -46,4 +53,8 @@ class DataIngestion:
 
 if __name__  == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data= obj.initiate_data_ingestion()
+    
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
